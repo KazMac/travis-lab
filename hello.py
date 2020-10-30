38d745a169089899210647e6e9476ba710861c66
Karen Mackay <1890991@sms.ed.ac.uk>
2020-10-30 14:55:34 +0000

    Add functions

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+def inputUserChoice():
+    name = prompt("Enter name: ")
+    return name
+
+def createMessage(name_msg)
+    message_created = "Hello " + name_msg
+    return message_created
+
+def printMessage(output_message):
+    print(output_message)
+
+name = inputUserChoice()
+message = createMessage()
+printMessage(message)
